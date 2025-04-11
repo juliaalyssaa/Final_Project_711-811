b93b2db66f1935e7fb75497e3f5aab980646a10b
Kayla Royce <kgr1020@ron.sr.unh.edu>
2025-04-11 16:13:03 -0400

    added to repo

--- final.sh
+++ final.sh
@@ -1,21 +1,23 @@
 #! /usr/bin/bash
 
-#files used: /tmp/qiimme2_dataset/manifest.tsv + metadata.tsv
 
-conda activate qiime2-amplicon-2024.5
+#files used: /tmp/qiimme2_dataset/manifest.tsv + metadata.tsv
+#date
+
+source activate qiime2-amplicon-2024.5
 
 mkdir -p ~/data/{data,results,scripts,visualizations,asv_tables}
 cd ~/data
 
-qimme tools import \
+qiime tools import \
  --type 'SampleData[PairedEndSequencesWithQuality]' \
  --input-path /home/users/kgr1020/Final_Project_711-811/data/manifest.tsv \
  --output-path demux.qza \
  --input-format PairedEndFastqManifestPhred33V2
 
-#qiime demux summarize
- #--i-data demux.qza
- #--o-visualization demux.qzv
+qiime demux summarize \
+ --i-data demux.qza \
+ --o-visualization demux.qzv
 
 #qiime dada2 denoise-paired \
  # --i-demultiplexed-seqs demux.qza \
