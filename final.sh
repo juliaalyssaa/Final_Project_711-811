a457a0889aeac0e492bb7dbff56c910776283a16
Kayla Royce <kgr1020@ron.sr.unh.edu>
2025-04-11 20:00:23 -0400

    added to script

--- final.sh
+++ final.sh
@@ -1,32 +1,60 @@
 #! /usr/bin/bash
-
+maindir="/home/users/kgr1020/GEN711FinalProject"
+datadir="$maindir/qiime2.microbiomedata"
+rddir="$maindir/Final_Project_711-811/rawdata/manifest.tsv"
 
 #files used: /tmp/qiimme2_dataset/manifest.tsv + metadata.tsv
-#date
+
+date
 
 source activate qiime2-amplicon-2024.5
 
-mkdir -p ~/data/{data,results,scripts,visualizations,asv_tables}
-cd ~/data
+mkdir -p $datadir
+cd $datadir
 
+echo "importing sequences into qiime"
 qiime tools import \
  --type 'SampleData[PairedEndSequencesWithQuality]' \
- --input-path /home/users/kgr1020/Final_Project_711-811/data/manifest.tsv \
+ --input-path $rddir \
  --output-path demux.qza \
  --input-format PairedEndFastqManifestPhred33V2
 
+echo "converting to qzv file"
 qiime demux summarize \
  --i-data demux.qza \
- --o-visualization demux.qzv
+ --o-visualization $datadir/demux.qzv
 
-#qiime dada2 denoise-paired \
- # --i-demultiplexed-seqs demux.qza \
-  #--p-trim-left-f 0 \
-  #--p-trunc-len-f 250 \
-  #--p-trim-left-r 0 \
-  #--p-trunc-len-r 250 \
-  #--o-representative-sequences asv-seqs.qza \
-  #--o-table asv-table.qza \
-  #--o-denoising-stats stats.qza
+echo "filtering reads"
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs demux.qza \
+  --p-trim-left-f 0 \
+  --p-trunc-len-f 250 \
+  --p-trim-left-r 0 \
+  --p-trunc-len-r 250 \
+  --o-representative-sequences asv-seqs.qza \
+  --o-table asv-table.qza \
+  --o-denoising-stats stats.qza
 
+echo "performing feature-table summarize action"
+qiime feature-table summarize-plus \
+  --i-table asv-table.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --o-summary asv-table.qzv \
+  --o-sample-frequencies sample-frequencies.qza \
+  --o-feature-frequencies asv-frequencies.qza
 
+echo "performing tabulate-seqs action"
+qiime feature-table tabulate-seqs \
+  --i-data asv-seqs.qza \
+  --m-metadata-file asv-frequencies.qza \
+  --o-visualization asv-seqs.qzv
+
+#qiime feature-table filter-features \
+#  --i-table asv-table.qza \
+#  --p-min-samples 2 \
+#  --o-filtered-table asv-table-ms2.qza
+
+#qiime feature-table filter-seqs \
+#  --i-data asv-seqs.qza \
+#  --i-table asv-table-ms2.qza \
+#  --o-filtered-data asv-seqs-ms2.qza
